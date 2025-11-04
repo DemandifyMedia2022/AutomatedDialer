1503e7ab92fa53aac0f683eea533f21fd8544216
Nishiraj Mane <manenishiraj@gmail.com>
2025-11-04 17:52:01 +0530

    ENV Secured

--- apps/backend/src/config/env.ts
+++ apps/backend/src/config/env.ts
@@ -5,10 +5,10 @@ dotenv.config();
 export const env = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: parseInt(process.env.PORT || '4000', 10),
-  DATABASE_URL: process.env.DATABASE_URL || 'mysql://user:password@localhost:3306/automated_dialer',
-  SIP_WSS_URL: process.env.SIP_WSS_URL || 'wss://pbx2.telxio.com.sg:8089/ws',
-  SIP_DOMAIN: process.env.SIP_DOMAIN || 'pbx2.telxio.com.sg',
-  STUN_SERVER: process.env.STUN_SERVER || 'stun:stun.l.google.com:19302',
-  PUBLIC_BASE_URL: process.env.PUBLIC_BASE_URL || 'http://localhost:4000',
+  DATABASE_URL: process.env.DATABASE_URL ,
+  SIP_WSS_URL: process.env.SIP_WSS_URL ,
+  SIP_DOMAIN: process.env.SIP_DOMAIN ,
+  STUN_SERVER: process.env.STUN_SERVER ,
+  PUBLIC_BASE_URL: process.env.PUBLIC_BASE_URL ,
   RECORDINGS_DIR: process.env.RECORDINGS_DIR || 'uploads/recordings'
 };
