3ded7e58ea6c3481f58256b290de3906d6ddf755
Nishiraj Mane <manenishiraj@gmail.com>
2025-11-21 14:42:39 +0530

    Preferances

---
+++ apps/frontend/src/app/dashboard/manager/settings/preferences/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PreferencesPage() {
+  return (
+    <div>
+      <h1>PreferencesPage</h1>
+      <p>Find me in ./apps/frontend/src/app/dashboard/manager/settings/preferences/page.tsx</p>
+    </div>
+  );
+}
